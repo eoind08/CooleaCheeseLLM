4a38ba3f662aae7f23b8ba292abc50918fe8e119
Erbium <102967870+App1ePi@users.noreply.github.com>
2025-04-14 21:52:50 +0100

    Added torch.compile

--- train_gpt2.py
+++ train_gpt2.py
@@ -16,6 +16,7 @@ class CausalSelfAttention(nn.Module):
          self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
          # output projection
          self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+         self.c_proj.COOLEACHEESE_SCALE_INIT = 1
          # regularization
          self.n_head = config.n_head
          self.n_embd = config.n_embd
@@ -53,6 +54,7 @@ class MLP(nn.Module):
          # Unlike ReLU alwaus some activation which means theres always some adaptation by the model
          self.gelu    = nn.GELU(approximate='tanh') 
          self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+         self.c_proj.COOLEACHEESE_SCALE_INIT = 1
  
      def forward(self, x):
          x = self.c_fc(x)
@@ -97,6 +99,23 @@ class GPT(nn.Module):
         ln_f = nn.LayerNorm(config.n_embd), # final layer
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # projects from 768 (number of embeddings) to 50000 (vocab size)
+
+        #weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+        #init params
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'COOLEACHEESE_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.2)
 
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
@@ -200,6 +219,8 @@ class DataLoaderLite:
         return x, y
 #-----------------------------------------------------------------------------------------------------------------------------------------------------
 # attempt to autodetect the device
+import time
+
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -207,22 +228,34 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
-train_loader = DataLoaderLite(B=4, T=32)
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+
+train_loader = DataLoaderLite(B=8, T=1024)
+
+torch.set_float32_matmul_precision('high')
 
 #get logits
 model = GPT(GPTConfig())
 model.to(device)
+model = torch.compile(model)
 
 # optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1-t0)*1000 # difference in miliseconds
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1 - t0)
+    print(f"step {i}, loss: {loss.item()}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
